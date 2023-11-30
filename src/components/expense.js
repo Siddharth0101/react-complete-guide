a3592500d145afeb9d9dcc5c7c69e16c8c5e3285
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2023-11-30 13:43:28 +0530

    basic

---
+++ src/components/expense.js
@@ -0,0 +1,12 @@
+function Expense() {
+  return (
+    <div>
+      <div>Dec 2023</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>200$</div>
+      </div>
+    </div>
+  );
+}
+export default Expense;
